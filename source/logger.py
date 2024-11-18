3d53b86b80589952a0bd614b911ace2f520f52e3
d3f4u1t <mushfikurahmaan@gmail.com>
2024-11-18 09:11:12 +0600

    notebook experiment done

--- source/logger.py
+++ source/logger.py
@@ -2,8 +2,8 @@ import logging
 import os
 from datetime import datetime
 
-LOG_FILE = f"{datetime.now().strftime('%m_%d_%Y_%H_%M_%S')}.log"
-logs_path = os.path.join(os.getcwd(),"logs",LOG_FILE)
+LOG_FILE = f"log_{datetime.now().strftime('%Y-%m-%d_%H-%M-%S')}.log"
+logs_path = os.path.join(os.getcwd(),"Logs",LOG_FILE)
 os.makedirs(logs_path, exist_ok=True)
 
 
@@ -11,7 +11,7 @@ LOG_FILE_PATH = os.path.join(logs_path, LOG_FILE)
 
 logging.basicConfig(
     filename=LOG_FILE_PATH,
-    format="[ %(asctime)s ] %(lineno)d %(name)s - %(levelname)s - %(message)s",
+    format="[ %(asctime)s ] %(levelname)s : %(module)s : %(lineno)d %(name)s : %(message)s",
     level=logging.INFO,
 
 )
